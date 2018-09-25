c3f7e4fcff56d7c07cab2853f2722a009a4879e3
Adrian <1998adrian98@gmail.com>
2018-09-25 16:17:49 +0300

    1

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
 module.exports = function solveEquation(equation) {
-  // your implementation
+
+  const abc = equation.split(/\s\*\sx(?:\^2)*/g);
+
+  const a = parseInt(abc[0].replace(/\s/g, ''));
+
+  const b = parseInt(abc[1].replace(/\s/g, ''));
+
+  const c = parseInt(abc[2].replace(/\s/g, ''));
+
+  const d = Math.pow(b, 2) - 4 * a *c;
+
+ 
+
+  const result = [];
+
+ 
+
+  if (d > 0) {
+
+                const x1 = Math.round((-b + Math.sqrt(d)) / (2 * a));
+
+                const x2 = Math.round((-b - Math.sqrt(d)) / (2 * a));
+
+                if (x1 > x2) {
+
+                                result.push(x2, x1);
+
+                } else {
+
+                                result.push(x1, x2);
+
+                }
+
+  } else if (!d) {
+
+                                result.push(-b / (2 * a));
+
+  }
+
+ 
+
+  return result;
+
 }
